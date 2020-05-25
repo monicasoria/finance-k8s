d90c2a69aea6e792326a0b3b3da038e05d194c93
DavidSantander <santander.david.19@gmail.com>
2020-05-25 16:37:48 -0500

    Add working function

--- flask-test-server/gcloud-test-function.py
+++ flask-test-server/gcloud-test-function.py
@@ -1,44 +1,11 @@
-import numpy
-import tensorflow
-from google.cloud import storage
-from tensorflow.keras.layers import Dense
-from tensorflow.keras import Model
-from pickle import load
+import numpy as np
 import os
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Sequential
+from pickle import load
 from flask import Flask
 from flask import request
-
 app = Flask(__name__)
-
-# We keep model as global variable so we don't have to reload it in case of warm invocations
-kickstarter_model = None
-
-
-class CustomModel(Model):
-    def __init__(self):
-        super(CustomModel, self).__init__()
-        self.relu1 = Dense(50, activation='relu')
-        self.relu2 = Dense(50, activation='relu')
-        self.d2 = Dense(2, activation='softmax')
-
-    def call(self, x):
-        x = self.relu1(x)
-        x = self.relu2(x)
-        return self.d2(x)
-
-
-def download_blob(bucket_name, source_blob_name, destination_file_name):
-    print('Downloading')
-    """Downloads a blob from the bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-
-    blob.download_to_filename(destination_file_name)
-
-    print('Blob {} downloaded to {}.'.format(
-        source_blob_name,
-        destination_file_name))
 
 
 def one_hot_encoding(raw_prediction_input, categorical_list, category_key):
@@ -51,9 +18,24 @@ def one_hot_encoding(raw_prediction_input, categorical_list, category_key):
     return raw_prediction_input
 
 
-@app.route('/predict', methods=['POST'])
+def create_model():
+    input_size = 10
+    output_size = 2
+    hidden_layer_size = 50
+    model = Sequential([
+        Dense(hidden_layer_size, activation='relu'),
+        Dense(hidden_layer_size, activation='relu'),
+        Dense(output_size, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def kickstarter_predict():
-    global kickstarter_model
+    request_json = request.json
+    print(request_json)
     # Define in a list the categories for prediction dictionary
     country_categories = ['country_GB', 'country_Other', 'country_US']
     main_categories = [
@@ -73,34 +55,25 @@ def kickstarter_predict():
         'main_category_Technology',
         'main_category_Theater'
     ]
-    # Scaler load
-    download_blob('finance-model-bucket', 'sklearn/scaler_kickstarter_v1.pkl',
-                  '/tmp/scaler_kickstarter_v1.pkl')
-    print('Scaler downloaded')
-    # Model load which only happens during cold starts
-    if kickstarter_model is None:
-        download_blob('finance-model-bucket', 'tensorflow/kickstarter_v1_weights.index.index',
-                      '/tmp/kickstarter_v1_weights.index.index')
-        download_blob('finance-model-bucket', 'tensorflow/kickstarter_v1_weights.data-00000-of-00001',
-                      '/tmp/kickstarter_v1_weights.data-00000-of-00001')
-        kickstarter_model = CustomModel()
-        kickstarter_model.load_weights('/tmp/kickstarter_v1_weights')
     # One hot encode Input
     country_one_hot_input = one_hot_encoding(
-        request.json, country_categories, 'country')
+        request_json, country_categories, 'country')
     complete_one_hot_input = one_hot_encoding(
-        request.json, main_categories, 'main_category')
+        request_json, main_categories, 'main_category')
     # Transform dictionary to list
     to_predict_input_list = []
     for key, value in complete_one_hot_input.items():
         temp = value
         to_predict_input_list.append(temp)
     # Load scaler
-    scaler = load(open('/tmp/scaler_kickstarter_v1.pkl', 'rb'))
+    scaler = load(open('scaler_kickstarter_v1.pkl', 'rb'))
     # Convert list to Numpy Array
     to_predict_input_array = np.array([to_predict_input_list])
     # Standardize the array
     standardized_input_array = scaler.transform(to_predict_input_array)
+    # Create and load weights
+    kickstarter_model = create_model()
+    kickstarter_model.load_weights('kickstarter_v1_weights')
     # Predict the input with the trained model
     prediction = kickstarter_model.predict(standardized_input_array)
     prediction_probability = round(np.float64(prediction[0, 1]), 2)*100
